5af422b4e22ea1ae6e741c8a60312546c7477f92
AKNL <aknlsame@gmail.com>
2023-11-24 11:43:23 +0100

    exo9

---
+++ exercice9.js
@@ -0,0 +1,6 @@
+let user = prompt("Entrez un nombre");
+
+for (let i = 1; i <=10; i++){
+    let result = user*i;
+    console.log(result);
+}
--- exercice_5.js
+++ exercice_5.js
@@ -3,6 +3,9 @@
 let firstNumber = prompt ("Veuillez entrez un nombre à virgule");
 let secondNumber = prompt ("Veuillez entrez un deuxiéme nombre à virgule");
 
+console.log(firstNumber);
+console.log(secondNumber);
+
 Math.trunc(firstNumber,secondNumber);
 
 let operation = 
