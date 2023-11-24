17cad1234fe81af648220bf42f13fd24fbcd9790
AKNL <aknlsame@gmail.com>
2023-11-24 10:33:57 +0100

    exo8

---
+++ exercice_5.js
@@ -0,0 +1,17 @@
+
+
+let firstNumber = prompt ("Veuillez entrez un nombre à virgule");
+let secondNumber = prompt ("Veuillez entrez un deuxiéme nombre à virgule");
+
+Math.trunc(firstNumber,secondNumber);
+
+let operation = 
+    function multiplication (firstNumber,secondNumber) {
+        return firstNumber*secondNumber;
+    }
+
+
+alert=(operation [multiplication]);
+
+
+
